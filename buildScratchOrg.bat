1d13855a88bd899be8777daf4228401a13817c37
LawrenceLoz <lawrencenewcombe@hotmail.co.uk>
2020-04-10 12:04:47 +0100

    Scratch org debug settings

--- buildScratchOrg.bat
+++ buildScratchOrg.bat
@@ -20,5 +20,7 @@ echo Pushed source
 call sfdx force:user:permset:assign --permsetname FormulaShare_Admin_User
 call sfdx force:user:permset:assign --permsetname FormulaShare_Sample_App_Permissions
 echo Assigned permissions
+call sfdx force:apex:execute -f config/setDebugModeForUser.apex
+echo Set up user for debug mode
 call sfdx force:apex:execute -f config/runApexOnInstallation.apex
 echo Created test data
