cf7af7cca4c7cd8c8e22f2bd38b0d0e119b293c3
Christian Schwabe <Christian.Schwabe@gmx.com>
2020-12-21 14:25:14 +0100

    Initial commit for bash script.

---
+++ buildScratchOrg.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Before running this script, make it executable with the following command:
+#chmod +x ./buildScratchOrg.sh
+
+username=formulasharetestuser%orgName%@sfdx.org
+nameOfMonth=$(date +%b)
+day=$(date +%d)
+
+orgName="${day}${nameOfMonth}FS"
+echo Username for default org: ${orgName}
+
+sfdx force:org:create -f config/project-scratch-def.json -a ${orgName} --setdefaultusername
+echo Created org with default username ${orgName}
+sfdx force:source:push
+echo Pushed source
+sfdx force:user:permset:assign --permsetname FormulaShare_Admin_User
+sfdx force:user:permset:assign --permsetname FormulaShare_Sample_App_Permissions
+echo Assigned permissions
+sfdx force:apex:execute -f config/setDebugModeForUser.apex
+echo Set up user for debug mode
+sfdx force:apex:execute -f config/runApexOnInstallation.apex
+echo Created test data
+sfdx force:user:create --setalias fstest --definitionfile config/user-def.json username=${username}
+echo Created test user fstest
