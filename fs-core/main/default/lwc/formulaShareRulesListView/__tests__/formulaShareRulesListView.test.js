ffa0987fa9a3d8c43c5a80a1aff19f22737856b3
Christian Schwabe <Christian.Schwabe@gmx.com>
2020-12-22 14:49:54 +0100

    Clean up test, add additional test.

--- fs-core/main/default/lwc/formulaShareRulesListView/__tests__/formulaShareRulesListView.test.js
+++ fs-core/main/default/lwc/formulaShareRulesListView/__tests__/formulaShareRulesListView.test.js
@@ -6,17 +6,6 @@ import { registerLdsTestWireAdapter } from '@salesforce/sfdx-lwc-jest';
 import { registerApexTestWireAdapter } from '@salesforce/sfdx-lwc-jest';
 import { getTreeGridData } from '@salesforce/apex/FormulaShareRulesListViewController.getTreeGridData';
 
-// Mocking imperative Apex method call.
-/*jest.mock(
-    '@salesforce/apex/FormulaShareRulesListViewController.getTreeGridData',
-    () => {
-        return {
-            default: jest.fn()
-        };
-    },
-    { virtual: true }
-);*/
-
 // Register as Apex wire adapter. Some tests verify that provisioned values trigger desired behavior.
 const getTreeGridListAdapter = registerApexTestWireAdapter(getTreeGridData);
 
@@ -26,14 +15,6 @@ const onlyParentRows = require('./data/onlyParentRows.json');
 
 // Register a test wire adapter.
 const getTreeGridDataWireAdapter = registerLdsTestWireAdapter(getTreeGridData);
-
-// Sample error for imperative Apex call
-/*const APEX_FORMULA_SHARE_RULES_ERROR = {
-    body: { message: 'An internal server error has occurred' },
-    ok: false,
-    status: 400,
-    statusText: 'Bad Request'
-};*/
 
 describe('c-formula-share-rules-list-view', () => {
     afterEach(() => {
@@ -59,31 +40,6 @@ describe('c-formula-share-rules-list-view', () => {
         return Promise.resolve().then(() => {
             const treeGrid = element.shadowRoot.querySelector('lightning-tree-grid');
             expect(treeGrid).toBeNull();
-        });
-    });
-
-    it('Test row with example data + columns in lightning-tree-grid (Positive).', () => {
-        // Create initial lwc element and attach to virtual DOM.
-        const element = createElement('c-formula-share-rules-list-view', {
-            is: FormulaShareRulesListView
-        });
-        document.body.appendChild(element);
-
-        getTreeGridDataWireAdapter.emit(mockExampleTreeGridData);
-        
-        // Resolve a promise to wait for a rerender of the new content.
-        return Promise.resolve().then(() => {
-            const treeGrid = element.shadowRoot.querySelector('lightning-tree-grid');
-            const parents = treeGrid.data;
-            const firstChild = parents[0]._children;
-            const secondChild = parents[1]._children;
-
-            expect(parents).toHaveLength(2);
-            expect(firstChild).toHaveLength(2);
-            expect(secondChild).toHaveLength(4);
-            
-            const columns = treeGrid.columns;
-            expect(columns).toHaveLength(8);
         });
     });
 
@@ -143,6 +99,57 @@ describe('c-formula-share-rules-list-view', () => {
             expect(handler.mock.calls[0][0].detail.title).toBe(TOAST_TITLE);
             expect(handler.mock.calls[0][0].detail.message).toEqual(expect.stringContaining(TOAST_MESSAGE));
             expect(handler.mock.calls[0][0].detail.variant).toBe(TOAST_VARIANT);
+        })
+    });
+
+    it('Test opening/closing modal c-formula-share-schedule-job-description (Positive).', () => {
+        // Create initial lwc element and attach to virtual DOM.
+        const element = createElement('c-formula-share-rules-list-view', {
+            is: FormulaShareRulesListView
+        });
+        document.body.appendChild(element);
+
+        // Mock data.
+        getTreeGridDataWireAdapter.emit(mockExampleTreeGridData);
+
+        // Return a promise to wait for any asynchronous DOM updates. Jest
+        // will automatically wait for the Promise chain to complete before
+        // ending the test and fail the test if the promise rejects.
+        return Promise.resolve().then(() => {
+            // Select ligthning-tree-grid.
+            const treeGrid = element.shadowRoot.querySelector('lightning-tree-grid');
+
+            const rowActionEvent = new CustomEvent(
+                "rowaction", {
+                    detail: {
+                        action: { name: 'scheduleWarning' },
+                    }
+            });
+            
+            // Trigger row action in lightning-tree-grid.
+            treeGrid.dispatchEvent(rowActionEvent);
+        })
+        .then(() => {
+            // Test opening modal window of c-formula-share-schedule-job-description.
+            const scheduleJobDescriptionModal = element.shadowRoot.querySelector('c-formula-share-schedule-job-description');
+
+            expect(scheduleJobDescriptionModal).not.toBeNull();
+        })
+        .then(() => {
+            // Select c-formula-share-schedule-job-description.
+            const scheduleJobDescriptionModal = element.shadowRoot.querySelector('c-formula-share-schedule-job-description');
+
+            // Prepare close event.
+            const closeEvent = new CustomEvent('close');
+
+            // Trigger custom event in c-formula-share-schedule-job-description.
+            scheduleJobDescriptionModal.dispatchEvent(closeEvent);
+        })
+        .then(() => {
+            // Test if modal window is closed.
+            const scheduleJobDescriptionModal = element.shadowRoot.querySelector('c-formula-share-schedule-job-description');
+
+            expect(scheduleJobDescriptionModal).toBeNull();
         })
     });
 });
