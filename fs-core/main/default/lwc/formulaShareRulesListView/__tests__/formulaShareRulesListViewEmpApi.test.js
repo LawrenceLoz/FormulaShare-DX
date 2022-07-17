33afe340e8920558066e0ac8c1737726fbecfaa3
Lawrence Newcombe <32834730+LawrenceLoz@users.noreply.github.com>
2022-07-17 13:15:43 +0100

    Jest updates for TestWireAdapter deprecation https://github.com/salesforce/wire-service-jest-util/blob/master/docs/migrating-from-version-2.x-to-3.x.md

--- fs-core/main/default/lwc/formulaShareRulesListView/__tests__/formulaShareRulesListViewEmpApi.test.js
+++ fs-core/main/default/lwc/formulaShareRulesListView/__tests__/formulaShareRulesListViewEmpApi.test.js
@@ -1,18 +1,38 @@
 import { createElement } from 'lwc';
+import {setImmediate} from 'timers';
 import FormulaShareRulesListView from 'c/formulaShareRulesListView';
-import { registerLdsTestWireAdapter } from '@salesforce/sfdx-lwc-jest';
-import { getTreeGridData } from '@salesforce/apex/FormulaShareRulesListViewController.getTreeGridData';
+import getTreeGridData from '@salesforce/apex/FormulaShareRulesListViewController.getTreeGridData';
 import getNamespacePrefix from '@salesforce/apex/FormulaShareUtilities.getNamespacePrefix';
 
 import { jestMockPublish } from 'lightning/empApi';
 
-// Register a test wire adapter.
-const getTreeGridDataWireAdapter = registerLdsTestWireAdapter(getTreeGridData);
-const getPrefixWireAdapter = registerLdsTestWireAdapter(getNamespacePrefix);
-
 // Import mock data to send through the wire adapter.
 const mockExampleTreeGridData = require('./data/exampleTreeGridData.json');
 const NAMESPACE_PREFIX = 'sfds__';
+
+// Mock getTreeGridData Apex wire adapter
+jest.mock(
+    '@salesforce/apex/FormulaShareRulesListViewController.getTreeGridData',
+    () => {
+        const {
+            createApexTestWireAdapter
+        } = require('@salesforce/sfdx-lwc-jest');
+        return {
+            default: createApexTestWireAdapter(jest.fn())
+        };
+    },
+    { virtual: true } 
+);
+// Mock getNamespacePrefix imperitive function
+jest.mock(
+    '@salesforce/apex/FormulaShareUtilities.getNamespacePrefix',
+    () => {
+        return {
+            default: jest.fn()
+        };
+    },
+    { virtual: true }    
+);
 
 // eslint-disable-next-line no-undef
 const flushPromises = () => new Promise(setImmediate);
@@ -28,33 +48,36 @@ describe('c-formula-share-rules-list-view', () => {
         jest.clearAllMocks();
     });
 
-    it('Test activate events (Positive).', async () => {
-        // Create initial lwc element and attach to virtual DOM.
-        const element = createElement('c-formula-share-rules-list-view', {
-            is: FormulaShareRulesListView
-        });
-        document.body.appendChild(element);
+    describe('getTreeGridData @wire', () => {
+        it('Test activate events (Positive).', async () => {
 
-        getPrefixWireAdapter.emit(NAMESPACE_PREFIX);
-        getTreeGridDataWireAdapter.emit(mockExampleTreeGridData);
+            // Create initial lwc element and attach to virtual DOM.
+            const element = createElement('c-formula-share-rules-list-view', {
+                is: FormulaShareRulesListView
+            });
+            document.body.appendChild(element);
 
-        // Make sure async subscribe call in connectedCallback completes
-        await flushPromises();
+            getNamespacePrefix.mockResolvedValue(NAMESPACE_PREFIX);
+            getTreeGridData.emit(mockExampleTreeGridData);
 
-        // connectedCallback is now complete, but no Platform Events have 
-        // been published yet.  Make assertions here about the state of your 
-        // component prior to receiving the Platform Events.
+            // Make sure async subscribe call in connectedCallback completes
+            await flushPromises();
 
-        // Mock-publish a Platform Event and await the promise
-        await jestMockPublish('/event/FormulaShare_List_Update__e', {
-            data: {
-                payload: {
-                    Object_Label__c: 'Account',
-                    Type__c: 'activate'
+            // connectedCallback is now complete, but no Platform Events have 
+            // been published yet.  Make assertions here about the state of your 
+            // component prior to receiving the Platform Events.
+
+            // Mock-publish a Platform Event and await the promise
+            await jestMockPublish('/event/FormulaShare_List_Update__e', {
+                data: {
+                    payload: {
+                        Object_Label__c: 'Account',
+                        Type__c: 'activate'
+                    }
                 }
-            }
+            });
+            // Now any DOM updates that depend on the Platform Event should 
+            // have rendered; assert about them here.
         });
-        // Now any DOM updates that depend on the Platform Event should 
-        // have rendered; assert about them here.
     });
 });
